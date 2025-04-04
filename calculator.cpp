47cd729205c28560d1bab00c8bf069f954cd5743
Nihil383 <engyayman@aucegypt.edu>
2025-04-04 20:53:04 +0200

    Added addition and subtraction

---
+++ calculator.cpp
@@ -0,0 +1,9 @@
+template <typename T1, typename T2, typename T3 = decltype(T1{} + T2{})>
+T3 add(T1 a, T2 b) {
+	return a + b;
+}
+
+template <typename T1, typename T2, typename T3 = decltype(T1{} - T2{})>
+T3 sub(T1 a, T2 b) {
+	return a - b;
+}
